feb3ea5a835f3da9d072ada3ae89e98ee5fe1420
marineco <marinecolonge@gmail.com>
2016-09-30 12:14:56 +0200

    Jeu quatre quart clicker Click sur la photo + incrementation ok

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
+/* Quatre quarts Clicker 
+
+Réalisez un petit jeu nommé cookie clicker, le but étant de 
+détecter les click sur le cookie et d'incrémenter le contenu de 
+la div #count avec le compte total !
+
+Bonus
+
+Tous les 20 click, demandez à l'utilisateur de trouver une activité plus productive via un alert
+Un message différent tous les 20 clicks
+Un message aléatoire parmi une liste de messages */
+
 $(document).ready(function(){
-	//votre code
+	
+	$("img").click(function(){
+
+	ajout = parseInt($(".counter").text());
+	$(".counter").text(ajout+1);
+
+	});
 
 
+	// var cookieClicker = 0;
+
+	// $("button").click(function(){
+	// 	cookieClicker ++;
+
+	// });
 });
