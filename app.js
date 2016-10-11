18639f7e9b340185ea5e80a21b3999a03a4155b6
marineco <marinecolonge@gmail.com>
2016-10-11 22:43:30 +0200

    Nettoyage de mon code Retirer les console.log / enlever les instructions commentées / enlever les bouts de code correspondant à des variantes du code mais commentées

--- app.js
+++ app.js
@@ -1,43 +1,3 @@
-/* Quatre quarts Clicker 
-
-Réalisez un petit jeu nommé cookie clicker, le but étant de 
-détecter les click sur le cookie et d'incrémenter le contenu de 
-la div #count avec le compte total !
-
-Bonus
-
-Tous les 20 click, demandez à l'utilisateur de trouver une activité 
-plus productive via un alert
-Un message différent tous les 20 clicks
-Un message aléatoire parmi une liste de messages */
-
-/* MON OPTION PREMIERE : mais pbm lors car message d'alert à 0 au lieu d'avoir le premier message d'alerte à 20 */
-
-/*$(document).ready(function(){
-	
-
-
-	$("img").click(function(){
-
-	var ajout = parseInt($(".counter").text());
-	$(".counter").text(ajout+1);
-	console.log(ajout);
-
-	if (ajout % 20 === 0){
-		alert('Trouve une activité plus productive !')
-	}
-
-
-	});
-
-});*/
-
-
-
-/*OPTION 2 : sans bug à 0 pr le message d'alerte*/
-
-
-
 
 $(document).ready(function(){
 	
@@ -45,22 +5,13 @@ $(document).ready(function(){
 	var tableau = ['click encore', 'allez encore une fois', 'tu peux mieux faire', 'courage'];
 	var len = tableau.length;
 
-
 	$("img").click(function(){
 		ajout++;
 		$(".counter").text(ajout);
-		console.log(ajout);
 
-// Tous les 20 click, demandez à l'utilisateur de trouver une activité 
-// plus productive via un alert
+		if (ajout % 20 === 0){
 
-	if (ajout % 20 === 0){
-	// alert('Trouve une activité plus productive !')
-	// }
-
-// 2 derniers bonus 
-
-	alert(tableau[Math.floor(Math.random() * len)]);
-}
-});
+			alert(tableau[Math.floor(Math.random() * len)]);
+		}
+	});
 });
